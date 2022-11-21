1b596f2bb7fce609485350d6d2f310c06e667e33
AndyWyy <56649489+AndyWyy@users.noreply.github.com>
2022-11-21 15:25:00 +0800

    update matrix slice
    update matrix slice

--- spmm_test_multi.py
+++ spmm_test_multi.py
@@ -9,16 +9,20 @@ import time
 import scanpy as sc
 import os 
 
-def task_put(var,sub_matrix):
-    var.put(sub_matrix)
+
 
 if __name__ == '__main__':
     filename = sys.argv[1]
     input_matrix = scipy.io.mmread(filename)
     print(input_matrix)
     thread_num = 4
-    q = Queue.Queue(maxsize)
+    q = Queue.Queue(thread_num)
     sparse_input_matrix = convert_sparse_matrix_to_sparse_tensor(input_matrix)
     m = sparse_input_matrix.shape[0]
-    k = 1
+    k = 32
+    size = k / thread_num
     rhs = tf.random.uniform([m,k],0.0,2147483647.0,tf.float64)
+    for i in range(4):
+        tmp_matrix = rhs.slice(rhs,[i*size,0],[size,m])
+        q.put(tmp_matrix)
+    
