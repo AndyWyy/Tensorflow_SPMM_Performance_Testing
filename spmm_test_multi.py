1eed866991a43567b7d15ff771ab535b4c0e6cb0
AndyWyy <56649489+AndyWyy@users.noreply.github.com>
2022-11-21 16:27:34 +0800

    Update spmm_test_multi.py
    add work func

--- spmm_test_multi.py
+++ spmm_test_multi.py
@@ -10,6 +10,18 @@ import scanpy as sc
 import os 
 
 
+def convert_sparse_matrix_to_sparse_tensor(X):
+    coo = X.tocoo()
+    indices = np.mat([coo.row, coo.col]).transpose()
+    return tf.SparseTensor(indices, coo.data, coo.shape)
+
+def work(q, sparse_input_matrix):
+    while True:
+        if q.empty():
+            return
+        else:
+            rhs = q.get()
+            out_put = tf.sparse.sparse_dense_matmul(sparse_input_matrix,rhs)
 
 if __name__ == '__main__':
     filename = sys.argv[1]
@@ -22,7 +34,20 @@ if __name__ == '__main__':
     k = 32
     size = k / thread_num
     rhs = tf.random.uniform([m,k],0.0,2147483647.0,tf.float64)
-    for i in range(4):
+    threads = []
+    for i in range(thread_num):
         tmp_matrix = rhs.slice(rhs,[i*size,0],[size,m])
         q.put(tmp_matrix)
+    start_time = time.time()
+    for i in range (thread_num):
+        t = threading.Thread(work,(q,sparse_input_matrix))
+    for i in range (thread_num):
+        threads[i].start()
+    for i in range (thread_num):
+        threads[i].join()
+    end_time = time.time()
+    print('spmm total time : ',round(end_time - start_time, 10),'secs')
+
+
+
     
