bae6405b66be6d1da33bb382687657561dcd4ebe
AndyWyy <56649489+AndyWyy@users.noreply.github.com>
2022-11-21 17:32:52 +0800

    Update spmm_test_multi.py
    fix bugs

--- spmm_test_multi.py
+++ spmm_test_multi.py
@@ -1,6 +1,6 @@
-from queue import Queue
-from threading import threading
 from threading import currentThread
+import queue
+import threading
 import tensorflow as tf 
 import numpy as np
 import scipy as scipy
@@ -28,19 +28,22 @@ if __name__ == '__main__':
     input_matrix = scipy.io.mmread(filename)
     print(input_matrix)
     thread_num = 4
-    q = Queue.Queue(thread_num)
+    q = queue.Queue(thread_num)
     sparse_input_matrix = convert_sparse_matrix_to_sparse_tensor(input_matrix)
     m = sparse_input_matrix.shape[0]
     k = 32
     size = k / thread_num
     rhs = tf.random.uniform([m,k],0.0,2147483647.0,tf.float64)
     threads = []
+    print(rhs)
     for i in range(thread_num):
-        tmp_matrix = rhs.slice(rhs,[i*size,0],[size,m])
+        tmp_matrix = tf.slice(rhs,[int(0),int(i*size)],[int(rhs.shape[0]),int(size)])
         q.put(tmp_matrix)
+        print(i)
     start_time = time.time()
     for i in range (thread_num):
-        t = threading.Thread(work,(q,sparse_input_matrix))
+        t = threading.Thread(target=work,args=[q,sparse_input_matrix,])
+        threads.append(t)
     for i in range (thread_num):
         threads[i].start()
     for i in range (thread_num):
